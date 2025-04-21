a7ab290750ea4f398e8af9dce1d1d396b79d7f86
rosemarie <rosemarie@few.io>
2025-04-21 14:18:31 -0400

    swipe item to delete

--- components/Item.tsx
+++ components/Item.tsx
@@ -8,38 +8,63 @@ import {
   useWindowDimensions,
   Platform,
 } from 'react-native';
-import React, { useState } from 'react';
+import React from 'react';
+import Reanimated from 'react-native-reanimated';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import { SharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function ItemCard(props: any) {
-  const web = Platform.OS == 'web';
-
-  const { containerStyle, index, deleteItem, name, displayDate } = props;
+  const { index, deleteItem, name, displayDate } = props;
   const { width } = useWindowDimensions();
 
+  const rightAction = (
+    prog: SharedValue<number>,
+    drag: SharedValue<number>
+  ) => {
+    const styleAnimation = useAnimatedStyle(() => {
+      return {
+        transform: [{ translateX: drag.value + 100 }],
+      };
+    });
+    return (
+      <Reanimated.View style={[styleAnimation, styles.buttonsContainer]}>
+        <Pressable style={styles.button}>
+          <FontAwesome6 name='edit' size={24} color='#028df7' />
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => deleteItem(index)}>
+          <FontAwesome6 name='trash' size={24} color='#b50214' />
+        </Pressable>
+      </Reanimated.View>
+    );
+  };
+
   return (
-    <View style={[styles.container, containerStyle, { width: width }]}>
-      <View
-        style={{
-          borderColor: 'black',
-          borderWidth: 0.5,
-          padding: 10,
-          borderRadius: 10,
-        }}
+    <GestureHandlerRootView>
+      <ReanimatedSwipeable
+        containerStyle={styles.container}
+        friction={2}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={40}
+        renderRightActions={rightAction}
       >
-        <Text>{name}</Text>
-        <Text numberOfLines={1} style={{ width: width / 2 }}>
-          {/* TODO more dynamic i.e determine weeks, months, years from now */}
-          Expires:{displayDate} days from now
-        </Text>
-      </View>
-      <View style={styles.deleteButton}>
-        <Button
-          title='Delete'
-          color={web ? 'red' : 'white'}
-          onPress={() => deleteItem(index)}
-        />
-      </View>
-    </View>
+        <View
+          style={{
+            borderColor: 'black',
+            borderWidth: 0.5,
+            padding: 10,
+            borderRadius: 10,
+          }}
+        >
+          <Text>{name}</Text>
+          <Text numberOfLines={1} style={{ width: width / 1.5 }}>
+            {/* TODO more dynamic i.e determine weeks, months, years from now */}
+            Expires:{displayDate} days from now
+          </Text>
+        </View>
+      </ReanimatedSwipeable>
+    </GestureHandlerRootView>
   );
 }
 
@@ -51,6 +76,13 @@ const styles = StyleSheet.create({
     padding: 4,
     flexDirection: 'row',
   },
+  buttonsContainer: {
+    flexDirection: 'row',
+    width: 100,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
   title: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -61,4 +93,5 @@ const styles = StyleSheet.create({
     color: 'red',
     backgroundColor: 'red',
   },
+  button: { padding: 10 },
 });
